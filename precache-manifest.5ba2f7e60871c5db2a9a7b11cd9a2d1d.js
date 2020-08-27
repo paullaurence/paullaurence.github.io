90331cb88cf71fe97c4ef396a38dbd3034493ee3
Paul Laurence Sriarwut <paulsriarwut@gmail.com>
2020-08-28 01:36:05 +0800

    Portfolio v1

---
+++ precache-manifest.5ba2f7e60871c5db2a9a7b11cd9a2d1d.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "768b52fb4fd34d958de3639020e5b302",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a2d37c755a97cc54b3b",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "2df0a9805f87f479a1e9",
+    "url": "/static/css/main.b5822b67.chunk.css"
+  },
+  {
+    "revision": "6a2d37c755a97cc54b3b",
+    "url": "/static/js/2.0ff21b7c.chunk.js"
+  },
+  {
+    "revision": "0d2320288227308a2855900744658c5e",
+    "url": "/static/js/2.0ff21b7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2df0a9805f87f479a1e9",
+    "url": "/static/js/main.0e3a5afa.chunk.js"
+  },
+  {
+    "revision": "f1f2c1b98a0eeeadad98",
+    "url": "/static/js/runtime-main.ce339f38.js"
+  },
+  {
+    "revision": "560dd2146533ee802e61b199a7d09af8",
+    "url": "/static/media/Cacaolavezares - Copy.560dd214.png"
+  },
+  {
+    "revision": "7a3be8db2dd7b42f59c7cb99b714462c",
+    "url": "/static/media/CoffeShop_Home.7a3be8db.jpg"
+  },
+  {
+    "revision": "537299ed292ddeaefcea323f5d6198dc",
+    "url": "/static/media/CoffeShop_Login.537299ed.png"
+  },
+  {
+    "revision": "3ca1b709f1e79ae9369da92012a52cd5",
+    "url": "/static/media/CoffeShop_Register.3ca1b709.png"
+  },
+  {
+    "revision": "f87e952d5789c3ba0bcd0911f68495a2",
+    "url": "/static/media/Devwork.f87e952d.png"
+  },
+  {
+    "revision": "df1c78936af535ff880f5f565f3a9577",
+    "url": "/static/media/Medicare Appointment.df1c7893.png"
+  },
+  {
+    "revision": "55c66eeaf8c02fd0f0f606c1e74d98b0",
+    "url": "/static/media/Medicare Login.55c66eea.png"
+  },
+  {
+    "revision": "5fa898d55e5c8e3dabb9ffc147e3cfc6",
+    "url": "/static/media/WebRaccoon Clothing_Home.5fa898d5.png"
+  },
+  {
+    "revision": "c82b06f05cc8ea0a7c916c15fcd95e83",
+    "url": "/static/media/Wertuu.c82b06f0.png"
+  },
+  {
+    "revision": "83d9c3d73946c88154ebfd1947f2d689",
+    "url": "/static/media/codeigniter-w.83d9c3d7.svg"
+  },
+  {
+    "revision": "440b9b231a84584c31de471bd31d0985",
+    "url": "/static/media/default.440b9b23.jpg"
+  },
+  {
+    "revision": "3cdd22bb30bb388f40a4d6079fae92d1",
+    "url": "/static/media/firebase-w.3cdd22bb.svg"
+  },
+  {
+    "revision": "7fb2e412dd5cc8c0586e3780f01eb9f5",
+    "url": "/static/media/graphql-c.7fb2e412.svg"
+  },
+  {
+    "revision": "7bb8e0ac7258dd703fd3b5d163216dcf",
+    "url": "/static/media/javascript-w.7bb8e0ac.svg"
+  },
+  {
+    "revision": "4415240233f58db97cf10eb6b9897f25",
+    "url": "/static/media/laravel-w.44152402.svg"
+  },
+  {
+    "revision": "9ba92bd7c1e044700f2f2884652c8d3e",
+    "url": "/static/media/laravel.9ba92bd7.svg"
+  },
+  {
+    "revision": "77a0055021e78a400e1c56f3c9f92e3e",
+    "url": "/static/media/long-dash-border.77a00550.svg"
+  },
+  {
+    "revision": "7f1096951958795ad19033ce7d0344cc",
+    "url": "/static/media/node-js-w.7f109695.svg"
+  },
+  {
+    "revision": "4337687002ea919ee3f4dc6c8853c576",
+    "url": "/static/media/nodejs-icon.43376870.svg"
+  },
+  {
+    "revision": "d4880bb1ea8e6aa2e28b923e826815f2",
+    "url": "/static/media/paul1.d4880bb1.png"
+  },
+  {
+    "revision": "5407b65864917086b15c81aca8de06a4",
+    "url": "/static/media/php-elephant-w.5407b658.svg"
+  },
+  {
+    "revision": "7daf3d1eb8906d95edbd49aa834b6938",
+    "url": "/static/media/react.7daf3d1e.svg"
+  },
+  {
+    "revision": "edddf48fb434ec6b8f0fac02feb3768a",
+    "url": "/static/media/redux-w.edddf48f.svg"
+  },
+  {
+    "revision": "56be21f010fbfd6aff3cba8245940014",
+    "url": "/static/media/vue-dot-js-w.56be21f0.svg"
+  },
+  {
+    "revision": "e33f3b82878d5d175b7bcc2576c5d259",
+    "url": "/static/media/vue.e33f3b82.svg"
+  },
+  {
+    "revision": "4886cafe09638594eb3bd20491882ae7",
+    "url": "/static/media/wordpress-w.4886cafe.svg"
+  }
+]);
